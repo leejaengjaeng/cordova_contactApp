a86d389efb05e1a0eeedbddfc70a912f39340249
?? ? <?? ?>
2016-08-01 14:44:38 +0900

    aa

--- www/js/index.js
+++ www/js/index.js
@@ -97,29 +97,32 @@ app.initialize();
 // });
 
 
-
+var smsnum;
+var cellphone;
 //alert(localStorage.length)
 function initial() {
     //localStorage.clear();
     if (localStorage.length != 0) {
-        var a = localStorage.getItem('cellphone1');
-        var b = localStorage.getItem('cellphone2');
-        var c = localStorage.getItem('cellphone3');
-        //alert(b);
-        var selectlen = document.getElementById("num1").length;
-
-        for (var i = 0; i < selectlen.length; i++) {
-            if (selectlen.option[i].value == a) {
-                selectlen.selected = 'selected';
-            }
-        }
-        document.getElementById("num2").value = b;
-        document.getElementById("num3").value = c;
-        // document.getElementById("loginbtn").click();
+        var cellphone = localStorage.getItem('cellphone');
+        var correct = localStorage.getItem('correct');
+        smsnum=correct;
+        cellphone=cellphone;
+        document.getElementById("correct").value = correct;
+        // //alert(b);
+        // var selectlen = document.getElementById("num1").length;
+        //
+        // for (var i = 0; i < selectlen.length; i++) {
+        //     if (selectlen.option[i].value == a) {
+        //         selectlen.selected = 'selected';
+        //     }
+        // }
+        // document.getElementById("num2").value = b;
+        // document.getElementById("num3").value = c;
+         document.getElementById("loginbtn2").click();
     }
 
-    document.getElementById("back").style.visibility = "hidden";
-    document.getElementById("body2").style.visibility = "hidden";
+    // document.getElementById("back").style.visibility = "hidden";
+    // document.getElementById("body2").style.visibility = "hidden";
 }
 
 function len_chk(){
@@ -144,18 +147,21 @@ function changenumber ( email ) {
         document.form1.email3.value = email;
     }
 }
-var smsnum;
+
 function ClickSMS(){
     var cellphone1 = document.getElementById('num1').value+"-"+document.getElementById('num2').value+"-"+document.getElementById('num3').value;
     $.ajax({
         method: 'GET',
         url: 'http://contact.attocube.co.kr/api/checksms',
-        data: {'cellphone':cellphone1, 'csrfmiddlewaretoken': '{{ csrf_token }}',},
+        data: {'cellphone':cellphone1, 'test':1, 'csrfmiddlewaretoken': '{{ csrf_token }}',},
         success: function (data) {
 
             if(data.sms>99999&&data.sms<1000000){
                 alert(data.sms);
+                localStorage.setItem("cellphone",cellphone1);
+                localStorage.setItem("correct",data.sms);
                 smsnum=data.sms;
+                cellphone=cellphone1;
                 document.getElementById("phonenumber").innerHTML = cellphone1;
                 document.getElementById("back").style.visibility = "visible";
                 document.getElementById("body1").style.display = "none";
@@ -213,35 +219,36 @@ function retrysms(){
         }
     })
 }
+
 function ClickLogin(){
 
-    var cellphone1 = document.getElementById('num1').value+"-"+document.getElementById('num2').value+"-"+document.getElementById('num3').value;
-    alert(cellphone1);
+    // var cellphone1 = document.getElementById('num1').value+"-"+document.getElementById('num2').value+"-"+document.getElementById('num3').value;
+    var cellp=localStorage.getItem("cellphone");
+    
     $.ajax({
         method: 'GET',
         url: 'http://contact.attocube.co.kr/api/checkuser',
-        data: {'cellphone':cellphone1, 'csrfmiddlewaretoken': '{{ csrf_token }}',},
+        data: {'cellphone':cellp, 'correct':smsnum,'csrfmiddlewaretoken': '{{ csrf_token }}',},
         success: function (data) {
             if(data.success==true){
-                localStorage.setItem('cellphone1',document.getElementById("num1").value);
-                localStorage.setItem('cellphone2',document.getElementById("num2").value);
-                localStorage.setItem('cellphone3',document.getElementById("num3").value);
 
-                var cellphone = data.current.cellphone;
-                var name = data.current.name;
-                var company = data.current.company;
-                var member = data.current.member;
-                var addr = data.current.address;
-                var phone = data.current.phone;
-                var fax = data.current.fax;
-                var email = data.current.email;
-                localStorage.setItem('name',name);
-                localStorage.setItem('email',email);
-                localStorage.setItem('company',company);
-                localStorage.setItem('member',member);
-                localStorage.setItem('address',addr);
-                localStorage.setItem('fax',fax);
-                localStorage.setItem('phone',phone);
+                alert("인증되었습니다.");
+
+                // var cellphone = data.current.cellphone;
+                // var name = data.current.name;
+                // var company = data.current.company;
+                // var member = data.current.member;
+                // var addr = data.current.address;
+                // var phone = data.current.phone;
+                // var fax = data.current.fax;
+                // var email = data.current.email;
+                // localStorage.setItem('name',name);
+                // localStorage.setItem('email',email);
+                // localStorage.setItem('company',company);
+                // localStorage.setItem('member',member);
+                // localStorage.setItem('address',addr);
+                // localStorage.setItem('fax',fax);
+                // localStorage.setItem('phone',phone);
 
                 location.href="list.html";
             }
@@ -256,6 +263,6 @@ function ClickLogin(){
 }
 
 function ClickBack(){
-    document.getElementById("body1").style.display="block";
-    document.getElementById("body2").style.visibility="hidden";
+    // document.getElementById("body1").style.display="block";
+    // document.getElementById("body2").style.visibility="hidden";
 }
