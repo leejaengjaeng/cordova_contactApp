bf0c3a0218c0abaf93edcc644f9451346b717b8c
leejaengjaeng <leejaengjaeng@naver.com>
2016-08-02 13:53:19 +0900

    detail

--- www/js/list.js
+++ www/js/list.js
@@ -81,21 +81,65 @@ function MakeList(directors){
     {
         $('#resultList').append('\
             <div class="container-fluid row resultview">\
-                <div class="col-xs-3">\
-                    <img src="img/a1.png" class="img-rounded img-responsive" alt="Responsive image">\
+                <div class="col-xs-3 userimgDiv">\
+                    <img src="img/a1.png" class="img-rounded img-responsive userImg" alt="Responsive image">\
                 </div>\
-                <div class="col-xs-9">\
+                <div class="col-xs-6">\
                     <ul class="list-unstyled">\
                         <li style="font-size: x-large;">\
                             <b>' + directors[director].name + '</b>\
                         </li>\
                         <li style="font-size: large;">\
-                        ' + directors[director].company + '\
+                            ' + directors[director].company + '\
                         </li>\
                     </ul>\
+                </div>\
+                <div class="col-xs-3">\
+                    <button onclick="showDetail($(this))">\
+                            보기\
+                    </button>\
+                    <input type="hidden" name="member" value='+directors[director].member+' >\
+                    <input type="hidden" name="cellphone" value='+directors[director].cellphone+' >\
+                    <input type="hidden" name="fax" value='+directors[director].fax+' >\
+                    <input type="hidden" name="name" value='+directors[director].name+' >\
+                    <input type="hidden" name="do" value='+directors[director].do+' >\
+                    <input type="hidden" name="address" value='+directors[director].address+' >\
+                    <input type="hidden" name="si" value='+directors[director].si+' >\
+                    <input type="hidden" name="company" value='+directors[director].company+' >\
+                    <input type="hidden" name="email" value='+directors[director].email+' >\
+                    <input type="hidden" name="phone" value='+directors[director].phone+' >\
                 </div>\
             </div>\
         ');
     }
     return true;
+}
+
+function showDetail(selectedBtn)
+{
+    $('#listView').hide();
+    $('#detailView').show();
+    $('#detailPage').empty();
+    
+    $('#detailPage').append('\
+        <img class="img-rounded img-responsive" src="'+selectedBtn.parent('div').parent('div').children('.userimgDiv').children('img').attr('src')+'">\
+        <h1>'+selectedBtn.closest('div').children('input[name="member"]').attr('value')+'</h1>\
+        <h1>'+selectedBtn.closest('div').children('input[name="cellphone"]').attr('value')+'</h1>\
+        <h1>'+selectedBtn.closest('div').children('input[name="fax"]').attr('value')+'</h1>\
+        <h1>'+selectedBtn.closest('div').children('input[name="name"]').attr('value')+'</h1>\
+        <h1>'+selectedBtn.closest('div').children('input[name="do"]').attr('value')+'</h1>\
+        <h1>'+selectedBtn.closest('div').children('input[name="address"]').attr('value')+'</h1>\
+        <h1>'+selectedBtn.closest('div').children('input[name="si"]').attr('value')+'</h1>\
+        <h1>'+selectedBtn.closest('div').children('input[name="company"]').attr('value')+'</h1>\
+        <h1>'+selectedBtn.closest('div').children('input[name="email"]').attr('value')+'</h1>\
+        <h1>'+selectedBtn.closest('div').children('input[name="phone"]').attr('value')+'</h1>\
+    ')
+    $('#detailHeaderTxt').text(selectedBtn.closest('div').children('input[name="name"]').attr('value')+"님의 상세정보");
+    $('#detailHeaderTxt').css('font-size','8vw')
+}
+
+function backToList()
+{
+    $('#detailView').hide();
+    $('#listView').show();
 }
